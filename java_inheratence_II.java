1ebe85968c0436a21cb044c055abb80fb6da666b
Divyanshu singh <80775542+Divyanshu050303@users.noreply.github.com>
2021-12-29 20:59:18 +0530

    java_inheratence_II

---
+++ java_inheratence_II.java
@@ -0,0 +1,22 @@
+package com.company;
+
+class arithmetic{
+    public int add(int a, int b){
+        int sum=a+b;
+        return sum;
+    }
+}
+class adder1 extends arithmetic{
+    @Override
+    public int add(int a, int b) {
+        int sum=a+b;
+        return sum;
+    }
+}
+public class java_inheratence_II {
+    public static void main(String[] args) {
+
+    }
+}
+
+
