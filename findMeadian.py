561769bdd070ed6474c4bc4cd81942413102967d
Divyanshu singh <80775542+Divyanshu050303@users.noreply.github.com>
2022-01-02 19:25:06 +0530

    findMeadian

---
+++ findMeadian.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+#
+# Complete the 'findMedian' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def findMedian(arr):
+    arr.sort()
+    print(arr)
+    return arr[len(arr)//2]
+
+if __name__ == '__main__':
+    # fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = findMedian(arr)
+    print(result)
+    print(7//2)
+
+    # fptr.write(str(result) + '\n')
+    #
+    # fptr.close()
